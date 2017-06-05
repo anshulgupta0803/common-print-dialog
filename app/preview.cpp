2bacd19b5346df053fe7a5a04270bdc22de0a4e5
Anshul Gupta <anshulgupta0803@gmail.com>
2017-06-06 03:49:52 +0530

    filenameLength bug fix in preview.cpp

--- app/preview.cpp
+++ app/preview.cpp
@@ -7,14 +7,14 @@ using namespace std;
 /* The preview works by using poppler to convert a page of pdf to QImage */
 
 QImage QPdfPreview::requestImage(const QString &id, QSize *size, const QSize &requested_size) {
-    int filenameLength = id.size() - id.indexOf("/") + 1;
+    int filenameLength = id.lastIndexOf("/");
     QString filename = "/" + id.mid(0, filenameLength);
     int pageNumber = id.mid(filenameLength + 1, id.size()).toInt();
 
     Poppler::Document *document = Poppler::Document::load(filename);
     if (!document || document->isLocked())
         cout << "ERROR1" << endl;
-    
+
     Poppler::Page *page = document->page(pageNumber);
     if (page == nullptr)
         cout << "ERROR2" << endl;
