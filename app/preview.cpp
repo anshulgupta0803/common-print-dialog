3cd6d744fd295dd68486e50e510f1f6b16d1a784
Anshul Gupta <anshulgupta0803@gmail.com>
2017-06-06 07:36:46 +0530

    Fixed crashing of dialog when next button in Preview exceeds the maximum pages.

--- app/preview.cpp
+++ app/preview.cpp
@@ -1,6 +1,8 @@
 #include "preview.h"
 #include <poppler/qt5/poppler-qt5.h>
 #include <iostream>
+#include <QtQuick/QQuickView>
+#include <QtQuick/QQuickItem>
 
 using namespace std;
 
@@ -10,18 +12,27 @@ QImage QPdfPreview::requestImage(const QString &id, QSize *size, const QSize &re
     int filenameLength = id.lastIndexOf("/");
     QString filename = "/" + id.mid(0, filenameLength);
     int pageNumber = id.mid(filenameLength + 1, id.size()).toInt();
+    QImage image;
 
     Poppler::Document *document = Poppler::Document::load(filename);
-    if (!document || document->isLocked())
-        cout << "ERROR1" << endl;
+    if (document && !document->isLocked()) {
 
-    Poppler::Page *page = document->page(pageNumber);
-    if (page == nullptr)
-        cout << "ERROR2" << endl;
-
-    QImage image = page->renderToImage(72.0, 72.0, 0, 0, page->pageSize().width(), page->pageSize().height());
-    if (image.isNull())
-        cout << "ERROR3" << endl;
+        Poppler::Page *page = document->page(pageNumber);
+        if (page != nullptr) {
+            image = page->renderToImage(72.0, 72.0, 0, 0, page->pageSize().width(), page->pageSize().height());
+            if (image.isNull())
+                cout << "ERROR3" << endl;
+        }
+//        else {
+//            QQuickView view;
+//            view.setSource(QUrl::fromLocalFile("PreviewForm.ui.qml"));
+//            QQuickItem *item = view.rootObject()->findChild<QQuickItem*>("image");
+//            if (item)
+//                item->setProperty("source", "Hello");
+//            else
+//                cout << "Error" << endl;
+//        }
+    }
 
     return image;
 }
