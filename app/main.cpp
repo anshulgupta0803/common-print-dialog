75a3580612088f0c6c5baee598c95122c8423b31
Anshul Gupta <anshulgupta0803@gmail.com>
2017-06-19 15:47:00 +0530

    Adding destinationModel to main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -2,6 +2,7 @@
 #include <QQmlApplicationEngine>
 #include "preview.h"
 #include <QQmlContext>
+#include <QStringList>
 
 int main(int argc, char *argv[]) {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -12,6 +13,13 @@ int main(int argc, char *argv[]) {
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
     QPreviewData data;
     engine.rootContext()->setContextProperty("preview_data", &data);
+
+    QStringList dataList;
+    dataList.append("Item 1");
+    dataList.append("Item 2");
+    dataList.append("Item 3");
+    dataList.append("Item 4");
+    engine.rootContext()->setContextProperty("destinationModel", QVariant::fromValue(dataList));
 
     return app.exec();
 }
